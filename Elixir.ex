5e7d9edb7fbb2d7b9ca308624c38723a996dbc68
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-08-24 11:08:15 +0900

    Create Elixir.ex

---
+++ Elixir.ex
@@ -0,0 +1,11 @@
+Erlang/OTP 24.0 [64-bit] [smp:2:2] [...]
+
+Interactive Elixir (1.15.4) - press Ctrl+C to exit
+iex> 40 + 2
+42
+iex> "hello" <> " world"
+"hello world"
+
+IO.puts("Hello world from Elixir")
+
+$ elixir simple.exs
